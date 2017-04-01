8cf82cf1ac3995387c1489429272710037d1865a
qiaobaoxi <1052913936@qq.com>
2017-04-01 17:59:01 +0800

    音曼普通用户完成

---
+++ pages/student/myCourse/myCourse.js
@@ -0,0 +1,30 @@
+// pages/student/myCourse/myCourse.js
+Page({
+  data: {
+    boff: true
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  },
+  switch: function (e) {
+    if (e.currentTarget.dataset.head == 1) {
+      this.setData({ boff: true })
+    } else {
+      this.setData({ boff: false })
+    }
+
+
+  }
+})
